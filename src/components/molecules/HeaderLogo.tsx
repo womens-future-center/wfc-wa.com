729b3608c032dc74a8a9ac0b8766b06b63edbe29
ryone9re <ryoichi.kurimoto@gmail.com>
2022-02-01 04:02:29 +0000

    ヘッダーロゴのデザインを追加

---
+++ src/components/molecules/HeaderLogo.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+import Logo from '../atoms/Logo'
+
+import styles from './styles/HeaderLogo.module.css'
+
+const HeaderLogo = () => {
+  return (
+    <Link href='/'>
+      <a>
+        <div className={styles.headerlogo}>
+          <Logo width={50} height={50} />
+        </div>
+      </a>
+    </Link>
+  )
+}
+
+export default HeaderLogo
