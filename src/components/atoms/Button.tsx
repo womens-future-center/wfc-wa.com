0bd5fdb4bcbf2d07337117c0dc86b6ed0ea2a63e
ryone9re <ryoichi.kurimoto@gmail.com>
2022-02-01 04:01:46 +0000

    クリック時のイベントリスナーを追加

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -2,10 +2,15 @@ import React, { ReactNode } from 'react'
 
 type Props = {
   children: ReactNode
+  onClick?: () => void
 }
 
-const Button = ({ children }: Props) => {
-  return <button type='button'>{children}</button>
+const Button = ({ children, onClick }: Props) => {
+  return (
+    <button onClick={onClick} type='button'>
+      {children}
+    </button>
+  )
 }
 
 export default Button
