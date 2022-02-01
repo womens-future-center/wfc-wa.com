8dde6d8e5030448b483aa8a82e169500b8182a8f
ryone9re <ryoichi.kurimoto@gmail.com>
2022-02-01 04:02:07 +0000

    ハンバーガーボタンのスタイルを追加

--- src/components/molecules/HamburgerButton.tsx
+++ src/components/molecules/HamburgerButton.tsx
@@ -4,10 +4,18 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 import Button from '../atoms/Button'
 
-const HamburgerButton = () => {
+import styles from './styles/HamburgerButton.module.css'
+
+type Props = {
+  onClick: () => void
+}
+
+const HamburgerButton = ({ onClick }: Props) => {
   return (
-    <Button>
-      <FontAwesomeIcon icon={faBars} />
+    <Button onClick={onClick}>
+      <div className={styles.hamburgerbutton}>
+        <FontAwesomeIcon icon={faBars} size='2x' />
+      </div>
     </Button>
   )
 }
