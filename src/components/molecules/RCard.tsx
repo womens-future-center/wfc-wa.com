9039ed9d7079819c506f57f1f62a2f5c5aeef7e4
ryone9re <ryoichi.kurimoto@gmail.com>
2022-05-15 14:28:18 +0000

    画像レスポンシブ対応

--- src/components/molecules/RCard.tsx
+++ src/components/molecules/RCard.tsx
@@ -3,24 +3,11 @@ import React from 'react'
 
 import { CardProps } from '../../types/types'
 
-const DEFAULT_IMAGE_HEIGHT = 1000
-
-const RCard = ({ imagePath, alt, title, content, imageWidth, imageHeight }: CardProps) => {
-  if (!imageWidth && !imageHeight) {
-    imageHeight = DEFAULT_IMAGE_HEIGHT
-    imageWidth = (imageHeight * 25) / 32
-  } else {
-    if (imageWidth) {
-      imageHeight = (imageWidth * 32) / 25
-    }
-    if (imageHeight) {
-      imageWidth = (imageHeight * 25) / 32
-    }
-  }
+const RCard = ({ imagePath, alt, title, content }: CardProps) => {
   return (
     <>
-      <div className='card w-full h-full flex flex-row bg-offwhite'>
-        <div className='p-5 flex-1 flex flex-col align-center'>
+      <div className='card lg:p-10 p-3 w-full h-full flex flex-row bg-offwhite'>
+        <div className='basis-2/3 lg:p-5 p-2 flex flex-col align-center'>
           <div>
             {typeof title === 'string' ? (
               <h1 className='lg:text-2xl text-xs text-center'>{title}</h1>
@@ -36,18 +23,16 @@ const RCard = ({ imagePath, alt, title, content, imageWidth, imageHeight }: Card
             )}
           </div>
         </div>
-        <figure className='p-5'>
-          <div className='flex-1 flex flex-row justify-center align-center'>
-            <Image
-              className='rounded-xl'
-              src={imagePath}
-              alt={alt}
-              width={imageWidth}
-              height={imageHeight}
-              layout='fixed'
-            />
-          </div>
-        </figure>
+        <div className='lg:m-5 m-2 basis-1/3 relative justify-center align-center'>
+          <Image
+            className='rounded-xl'
+            src={imagePath}
+            alt={alt}
+            layout='fill'
+            objectFit='contain'
+            priority={true}
+          />
+        </div>
       </div>
     </>
   )
--- src/components/molecules/VeriticalCard.tsx
+++ src/components/molecules/VeriticalCard.tsx
@@ -3,50 +3,35 @@ import React from 'react'
 
 import { CardProps } from '../../types/types'
 
-const DEFAULT_IMAGE_HEIGHT = 1000
-
-const VerticalCard = ({ imagePath, alt, title, content, imageWidth, imageHeight }: CardProps) => {
-  if (!imageWidth && !imageHeight) {
-    imageHeight = DEFAULT_IMAGE_HEIGHT
-    imageWidth = (imageHeight * 3) / 2
-  } else {
-    if (imageWidth) {
-      imageHeight = (imageWidth * 2) / 3
-    }
-    if (imageHeight) {
-      imageWidth = (imageHeight * 3) / 2
-    }
-  }
-  const titleWithComponent =
-    typeof title === 'string' ? (
-      <h1 className='lg:text-2xl text-xs text-center break-all'>{title}</h1>
-    ) : (
-      title
-    )
-  const contentWithComponent =
-    typeof content === 'string' ? (
-      <p className='lg:text-lg text-xs text-left break-all'>{content}</p>
-    ) : (
-      content
-    )
+const VerticalCard = ({ imagePath, alt, title, content }: CardProps) => {
   return (
     <>
-      <div className='card w-full h-full flex flex-col bg-offwhite'>
-        <figure className='p-5'>
-          <div className='flex-1 flex flex-row justify-center align-center'>
-            <Image
-              className='rounded-xl'
-              src={imagePath}
-              alt={alt}
-              width={imageWidth}
-              height={imageHeight}
-              layout='fixed'
-            />
+      <div className='card lg:p-10 p-3 w-full h-full flex flex-col bg-offwhite'>
+        <div className='lg:m-5 m-2 basis-1/3 relative justify-center align-center'>
+          <Image
+            className='rounded-xl'
+            src={imagePath}
+            alt={alt}
+            layout='fill'
+            objectFit='contain'
+            priority={true}
+          />
+        </div>
+        <div className='basis-2/3 lg:p-5 p-2 flex flex-col align-center'>
+          <div>
+            {typeof title === 'string' ? (
+              <h1 className='lg:text-2xl text-base text-center break-all'>{title}</h1>
+            ) : (
+              title
+            )}
           </div>
-        </figure>
-        <div className='p-5 flex-1 flex flex-col align-center'>
-          <div>{titleWithComponent}</div>
-          <div className='mt-3'>{contentWithComponent}</div>
+          <div className='mt-3'>
+            {typeof content === 'string' ? (
+              <p className='lg:text-lg text-sm text-left break-all'>{content}</p>
+            ) : (
+              content
+            )}
+          </div>
         </div>
       </div>
     </>
