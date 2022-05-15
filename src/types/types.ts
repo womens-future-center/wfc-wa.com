ad4df0e5c4264138f5c36b77f8c075b20cebb77e
ryone9re <ryoichi.kurimoto@gmail.com>
2022-05-15 14:30:26 +0000

    画像レスポンシブ対応に伴い画像サイズを渡さなくて良いように変更

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,9 +1,13 @@
 import { ReactNode } from 'react'
 
-export type BusinessList = {
-  imgPath: string
+export const TAILWIND_SM = 640
+export const TAILWIND_MD = 768
+export const TAILWIND_LG = 1024
+export const TAILWIND_XL = 1280
+export const TAILWIND_2XL = 1536
+
+export type CardContent = Omit<CardProps, 'title' | 'alt' | 'imageWidth' | 'imageHeight'> & {
   title: string
-  content: string
 }
 
 export type CardProps = {
@@ -11,6 +15,5 @@ export type CardProps = {
   alt: string
   title: string | ReactNode
   content: string | ReactNode
-  imageWidth?: number
-  imageHeight?: number
+  className?: string
 }
