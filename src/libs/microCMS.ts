0feb864273882163a8d2d247aab10e1e7c557b6c
ryone9re <ryoichi.kurimoto@gmail.com>
2022-05-19 11:30:31 +0000

    client fix

--- src/libs/microCMS.ts
+++ src/libs/microCMS.ts
@@ -1,16 +1,8 @@
 import { createClient } from 'microcms-js-sdk'
-import { MicroCMS } from 'microcms-lib'
 import { ImageLoaderProps } from 'next/image'
-
-import { EndPoints } from '../types/cms-types'
 
 export const client = createClient({
   serviceDomain: process.env.SERVICE_DOMAIN!,
-  apiKey: process.env.X_MICROCMS_API_KEY!,
-})
-
-export const cms = new MicroCMS<EndPoints>({
-  service: process.env.SERVICE_DOMAIN!,
   apiKey: process.env.X_MICROCMS_API_KEY!,
 })
 
