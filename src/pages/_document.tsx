3c6689e8fdce0363a5f6be2ecda831f9ec078c9d
ryone9re <ryoichi.kurimoto@gmail.com>
2022-05-19 08:57:44 +0000

    metaタグ追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,12 @@ export default function Document() {
           name='Description'
           content="女性の起業支援を行うWomen's Future Center。400名以上の女性ネットワーク会員が自主企画講座を開催! 在宅ワーカー支援事業も行っております｡"
         />
+        <meta
+          name='Description'
+          content="女性起業支援のWomen's Future Center(ウィメンズフューチャーセンター)
+          女性の起業支援を行うWomen's Future Center。400名以上の女性ネットワーク会員が自主企画講座を開催!
+          在宅ワーカー支援事業も行っております｡"
+        />
         <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
