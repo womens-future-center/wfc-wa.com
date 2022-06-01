cee2968352d48d72139a4c420f37f9dd697958b1
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-01 16:47:14 +0900

    テストページ追加

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -29,23 +29,15 @@ export default Test
 const Body = () => {
   return (
     <>
-      <div className='relative'>
+      <div className='relative md:h-1/2 h-2/3'>
         <motion.div
           animate={{
-            y: [20, -20, 20],
+            y: [10, -5, 10],
           }}
-          transition={{ repeat: Infinity, duration: 2.5 }}
-          className='relative'
+          transition={{ repeat: Infinity, duration: 3 }}
+          className='absolute w-5/6 h-5/6 md:left-1/4 left-[5%]'
         >
-          <div className='left-1/2 bottom-1/3'>
-            <Image
-              loader={microCMSLoader}
-              src='/top_image/man.png'
-              alt='Top image'
-              width={100}
-              height={100}
-            />
-          </div>
+          <Image loader={microCMSLoader} src='/top_image/man.png' alt='Top image' layout='fill' />
         </motion.div>
         <h1 className='xl:text-4xl md:text-3xl text-base absolute left-1/2 bottom-1/3'>
           誰もが自由な働き方ができる社会へ
